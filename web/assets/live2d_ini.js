d8eca500fccfb90456785192b4821a2eb24a51f0
Jerry <757320383@qq.com>
2019-05-22 10:16:35 +0800

    添加看板娘模型

---
+++ web/assets/live2d_ini.js
@@ -0,0 +1,25 @@
+String.prototype.render = function (context) {
+    var tokenReg = /(\\)?\{([^\{\}\\]+)(\\)?\}/g;
+
+    return this.replace(tokenReg, function (word, slash1, token, slash2) {
+        if (slash1 || slash2) {
+            return word.replace('\\', '');
+        }
+
+        var variables = token.replace(/\s/g, '').split('.');
+        var currentObject = context;
+        var i, length, variable;
+
+        for (i = 0, length = variables.length; i < length; ++i) {
+            variable = variables[i];
+            currentObject = currentObject[variable];
+            if (currentObject === undefined || currentObject === null) return '';
+        }
+        return currentObject;
+    });
+};
+
+function initModel(url) {
+    var path = url + 'L?model_name=random';
+    loadlive2d('live2d', path, console.log('live2d', '加载模型 ' + path));
+}
